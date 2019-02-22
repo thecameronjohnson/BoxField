3abc99f3e695f28c398802bc5de946f43784976f
cam johnson <the.camjohnson@gmail.com>
2019-02-22 09:54:08 -0500

    added player stuff

--- BoxField/Box.cs
+++ BoxField/Box.cs
@@ -3,22 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace BoxField
 {
     class Box
     {
-        //need colour eventually
+        //need colour eventually (done)
         public int x, y, size;
+        public Color colour;
 
-        public Box (int _x, int _y, int _size)
+        public Box (int _x, int _y, int _size, Color _colour)
         {
             x = _x;
             y = _y;
             size = _size;
-
+            colour = _colour;
         }
 
         //need a move method
+        public void Move(int speed)
+        {
+            y += speed;
+        }
+
+        public void Move(int speed, string direction)
+        {
+            if (direction == "right")
+            {
+                x += speed;
+            }
+            else if (direction == "left")
+            {
+                x -= speed;
+            }
+        }
+
+        public bool Collision(Box b)
+        {
+            Rectangle rec1 = new Rectangle(b.x, b.y, b.size, b.size);
+            Rectangle rec2 = new Rectangle(x, y, size, size);
+
+            if (rec1.IntersectsWith(rec2))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- BoxField/Screens/GameScreen.cs
+++ BoxField/Screens/GameScreen.cs
@@ -22,17 +22,83 @@ namespace BoxField
         List<Box> leftBoxes = new List<Box>();
         List<Box> rightBoxes = new List<Box>();
 
+        Box player;
+
+        Random randGen = new Random();
+
+        int boxCounter;
+        int boxSpeed = 5;
+
         public GameScreen()
         {
             InitializeComponent();
+            OnStart();
         }
+
+
+
+        public Color randomColour()
+        {
+            /*
+            int red = randGen.Next(0, 255);
+            int blue = randGen.Next(0, 255);
+            int green = randGen.Next(0, 255);
+            return Color.FromArgb(red, blue, green);*/
+            int red = 0;
+            int blue = 0;
+            int green = 0;
+            int randomSet = randGen.Next(0, 4);
+            string chosenColour = Convert.ToString(randomSet);
+
+            switch (chosenColour)
+            {
+                case "0":
+                    red = 0;
+                    green = 29;
+                    blue = 255;
+                    break;
+                case "1":
+                    red = 12;
+                    green = 109;
+                    blue = 232;
+                    break;
+                case "2":
+                    red = 0;
+                    green = 196;
+                    blue = 255;
+                    break;
+                case "3":
+                    red = 12;
+                    green = 232;
+                    blue = 210;
+                    break;
+                case "4":
+                    red = 0;
+                    green = 255;
+                    blue = 140;
+                    break;
+            }
+
+            return Color.FromArgb(red, green, blue);
+            }
 
         /// <summary>
         /// Set initial game values here
         /// </summary>
         public void OnStart()
         {
-            //TODO - set game start values
+            Color randomized = randomColour();
+
+            //set game start values
+            Box b1 = new Box(25, 24, 20, randomized);
+            leftBoxes.Add(b1);
+
+            Box b2 = new Box(leftBoxes[0].x + this.Height/3 , 24, 20, randomized);
+            rightBoxes.Add(b2);
+
+            Color playerColour = Color.FromArgb(255, 153, 0);
+
+            player = new Box(400, 450, 20, playerColour);
         }
 
         private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
@@ -65,18 +131,75 @@ namespace BoxField
 
         private void gameLoop_Tick(object sender, EventArgs e)
         {
-            //TODO - update location of all boxes (drop down screen)
+            //update location of all boxes (drop down screen)
+            foreach (Box b in leftBoxes)
+            {
+                b.Move(boxSpeed);
+            }
+            foreach (Box b in rightBoxes)
+            {
+                b.Move(boxSpeed);
+            }
 
-            //TODO - remove box if it has gone of screen
+            if (leftArrowDown == true && player.x > 0)
+            {
+                player.Move(5, "left");
+            }
 
-            //TODO - add new box if it is time
+            if (rightArrowDown == true && player.y <= this.Width - player.size)
+            {
+                player.Move(5, "right");
+            }
+
+            foreach(Box b in leftBoxes.Union(rightBoxes))
+            {
+                if (player.Collision(b))
+                {
+                    gameLoop.Stop();
+                }
+            }
+
+            //remove box if it has gone of screen
+
+            if (leftBoxes[0].y > this.Height - leftBoxes[0].size)
+        {
+                leftBoxes.RemoveAt(0);
+                rightBoxes.RemoveAt(0);
+        }
+           
+            //add new box if it is time
+            boxCounter++;
+            if (boxCounter % 5 == 0)
+            {
+                Color randomized = randomColour();
+
+                Box b1 = new Box(25, 24, 20, randomized);
+                leftBoxes.Add(b1);
+
+                Box b2 = new Box(b1.x + this.Height / 3, 24, 20, randomized);
+                rightBoxes.Add(b2);
+                boxCounter = 0;
+            }
 
             Refresh();
         }
 
         private void GameScreen_Paint(object sender, PaintEventArgs e)
         {
-            //TODO - draw boxes to screen
+            //draw boxes to screen
+            foreach(Box b in leftBoxes)
+            {
+                boxBrush.Color = b.colour;
+                e.Graphics.FillRectangle(boxBrush, b.x, b.y, b.size, b.size);
+            }
+            foreach (Box b in rightBoxes)
+            {
+                boxBrush.Color = b.colour;
+                e.Graphics.FillRectangle(boxBrush, b.x, b.y, b.size, b.size);
+            }
+
+            boxBrush.Color = player.colour;
+            e.Graphics.FillEllipse(boxBrush, player.x, player.y, player.size, player.size);
         }
     }
 }
